eb1999e790fd9e783e4f89bef17a85ce3234606c
epeshk <peshkov@skbkontur.ru>
2018-09-12 13:33:40 +0500

    constants for tests

---
+++ Vostok.ClusterClient.Transport.Webrequest.Tests/Consrants.cs
@@ -0,0 +1,10 @@
+namespace Vostok.ClusterClient.Transport.Webrequest.Tests
+{
+    internal static class Constants
+    {
+        public const long Kilobytes = 1024;
+        public const long Megabytes = 1024 * Kilobytes;
+        public const long Gigabytes = 1024 * Kilobytes;
+        
+    }
+}
