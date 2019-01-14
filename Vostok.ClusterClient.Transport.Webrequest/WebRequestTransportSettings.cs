ed82133da32f908671861d54f89d435f4e7dde27
iloktionov <iloktionov@skbkontur.ru>
2019-01-14 19:41:01 +0500

    WebRequestTransportSettings: adjusted default settings values.

--- Vostok.ClusterClient.Transport.Webrequest/WebRequestTransportSettings.cs
+++ Vostok.ClusterClient.Transport.Webrequest/WebRequestTransportSettings.cs
@@ -16,30 +16,30 @@ namespace Vostok.Clusterclient.Transport.Webrequest
 
         public TimeSpan RequestAbortTimeout { get; set; } = TimeSpan.FromMilliseconds(250);
 
-        public IWebProxy Proxy { get; set; } = null;
+        public IWebProxy Proxy { get; set; }
 
         public int MaxConnectionsPerEndpoint { get; set; } = 10 * 1000;
 
-        public long? MaxResponseBodySize { get; set; } = null;
+        public long? MaxResponseBodySize { get; set; }
 
         public Predicate<long?> UseResponseStreaming { get; set; } = _ => false;
 
-        public string ConnectionGroupName { get; set; } = null;
+        public string ConnectionGroupName { get; set; }
 
-        public bool AllowAutoRedirect { get; set; } = false;
+        public bool AllowAutoRedirect { get; set; }
 
-        public bool TcpKeepAliveEnabled { get; set; } = false;
+        public bool TcpKeepAliveEnabled { get; set; } = true;
 
         public TimeSpan TcpKeepAliveTime { get; set; } = TimeSpan.FromSeconds(3);
 
         public TimeSpan TcpKeepAliveInterval { get; set; } = TimeSpan.FromSeconds(1);
 
-        public bool ArpCacheWarmupEnabled { get; set; } = false;
+        public bool ArpCacheWarmupEnabled { get; set; } = true;
 
-        public X509Certificate2[] ClientCertificates { get; set; } = null;
+        public X509Certificate2[] ClientCertificates { get; set; }
 
         internal Func<int, byte[]> BufferFactory { get; set; } = size => new byte[size];
 
-        internal bool FixNonAsciiHeaders { get; set; } = false;
+        internal bool FixNonAsciiHeaders { get; set; }
     }
 }
